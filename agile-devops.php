de2b5aeb9d652927647ddccf488ced0ce9a16891
genewsoft <genewsoft>
2022-12-01 17:00:02 +0530

    agiles

---
+++ agile-devops.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="zxx">
+    <head>
+    <?php include 'common/head.php' ;?>
+		<!-- Title -->
+		<title>Nakoa Technologies</title>
+    </head>
+
+    <body>
+    <?php include 'common/header.php' ;?>
+		<!-- Start Page Title Area -->
+		<div class="page-title-area bg-23">
+			<div class="container">
+				<div class="page-title-content">
+					<h2>Services Details</h2>
+					<ul>
+						<li>
+							<a href="index.php">
+								Home 
+							</a>
+						</li>
+
+						<li>Services</li>
+						<li class="active">Services Details</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<!-- End Page Title Area -->
+		
+		<!-- Start Blog Details Area -->
+        <section class="blog-details-area ptb-100">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12 col-md-12">
+						<div class="services-details">
+							<h3>Agile & DevOps Solutions</h3>
+
+							<img src="assets/img/services-details/s8.jpg" alt="Image">
+
+							<p></p>
+                     <p></p>
+                     <p></p>
+                     <p></p>
+                     <p></p>
+                     <p></p>
+                     <p></p>
+					
+
+							<h3 class="services-related-post">Related Post</h3>
+							<div class="row">
+								<div class="col-lg-6 col-sm-6">
+									<div class="single-solutions solutions-time-bg-2 mb-0 mb-ud" style="background-image: url(assets/img/solution-img/s2.jpg);">
+                           <div class="solutions-content">
+										<h3>Web Development</h3>
+										<p>Want to start your business quickly? We help you to do it at a lightning fast. We create websites with rich UI / UX contents to draw in visitors. 
+</p>
+										<a href="web-development.php" class="read-more">
+											Read More
+										</a>
+									</div>
+									</div>
+								</div>
+			
+								<div class="col-lg-6 col-sm-6">
+									<div class="single-solutions solutions-time-bg-3 mb-0" style="background-image: url(assets/img/solution-img/s6.jpg);">
+                           <div class="solutions-content">
+								<h3>Enterprise Solutions</h3>
+								<p>With the help of our cutting-edge technological solutions, businesses are able to automate repetitive tasks, get rid of human error, improve operational efficiency and accuracy..</p>
+								<a href="enterprise-solutions.php" class="read-more">
+									Read More
+								</a>
+							</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</section>
+		<!-- End Blog Details Area -->
+
+      <?php include 'common/footer.php' ;?>
+
+    </body>
+</html>
